3c99d01b388741423b6914d067dd343ce94b38b2
Krzysiek Ruczaj <krj.public+github@gmail.com>
2019-12-05 02:23:35 +0100

    Add application startup point.

---
+++ src/main/java/com/staxter/StaxterApplication.java
@@ -0,0 +1,13 @@
+package com.staxter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaxterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaxterApplication.class, args);
+	}
+
+}
