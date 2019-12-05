f4717b9ea400a7b2348b245840c12d76668d4b91
Krzysiek Ruczaj <krj.public+github@gmail.com>
2019-12-05 02:31:56 +0100

    Add basic error DTO representation.

---
+++ src/main/java/com/staxter/web/ErrorDto.java
@@ -0,0 +1,19 @@
+package com.staxter.web;
+
+public class ErrorDto {
+    private String code;
+    private String message;
+
+    public ErrorDto(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
