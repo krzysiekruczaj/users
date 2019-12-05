fabca9037a71a830e6fc4a495c14baf5a30e0bc1
Krzysiek Ruczaj <krj.public+github@gmail.com>
2019-12-05 02:31:35 +0100

    Add basic user DTO representation.

---
+++ src/main/java/com/staxter/user/UserDtoConverter.java
@@ -0,0 +1,17 @@
+package com.staxter.user;
+
+import com.staxter.userrepository.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoConverter {
+
+    public UserDto convertToDto(User user) {
+        UserDto dto = new UserDto();
+        dto.setId(user.getId());
+        dto.setFirstName(user.getFirstName());
+        dto.setLastName(user.getLastName());
+        dto.setUserName(user.getUserName());
+        return dto;
+    }
+}
