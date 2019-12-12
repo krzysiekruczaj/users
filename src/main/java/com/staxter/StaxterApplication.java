5a43b48043cacc32d93246fa57b63b766bf641cd
Krzysztof Ruczaj <krzysztof.ruczaj@cumulocity.com>
2019-12-12 02:43:58 +0100

    Removed unnecessary configuration.

--- src/main/java/com/staxter/StaxterApplication.java
+++ src/main/java/com/staxter/StaxterApplication.java
@@ -1,9 +1,11 @@
 package com.staxter;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class StaxterApplication {
 
 	public static void main(String[] args) {
